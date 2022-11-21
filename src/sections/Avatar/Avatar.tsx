60cf9f45d4d0e77e6b52bf801bbf9ddc4a6edea0
Robin Boucher <robin.boucher@epita.fr>
2022-11-21 01:14:52 +0100

    wip

--- src/sections/Avatar/Avatar.tsx
+++ src/sections/Avatar/Avatar.tsx
@@ -1,26 +1,65 @@
-import { ActionIcon, Box, Button, Center, Group, Stack } from '@mantine/core'
-import React, { useContext } from 'react'
-import { Download, Repeat } from 'tabler-icons-react'
-import { TubavContext } from '../../TubavContext'
-import { Canvas } from './Canvas'
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Card,
+  Center,
+  Group,
+  Image,
+  Stack,
+} from "@mantine/core";
+import React, { useContext, useState } from "react";
+import { Download, Repeat } from "tabler-icons-react";
+import { TubavContext } from "../../TubavContext";
+import { Canvas } from "./Canvas";
+
+const ContextMenu: React.FC<{ x: number; y: number }> = ({ x, y }) => {
+  return (
+    <Card
+      style={{
+        height: "128px",
+        position: "absolute",
+        top: y + "px",
+        left: x + "px",
+      }}
+    >
+      <a href="shaigro.dev">Hello</a>
+    </Card>
+  );
+};
 
 /**
  * Displays avatar and button to download it.
  */
 export const Avatar = () => {
-  const { downloadAvatar } = useContext(TubavContext)
+  const { downloadAvatar } = useContext(TubavContext);
+  const [coordinates, setCoordinates] = useState({ x: -1, y: -1 });
 
   return (
-    <Stack justify="center">
-      <Box style={{ flex: 3, textAlign: 'center' }}>
-        <Canvas id="ub-avatar" />
-      </Box>
+    <React.Fragment>
+      <Stack justify="center">
+        <Box
+          onContextMenu={(event: {
+            pageX: number;
+            pageY: number;
+            preventDefault: () => void;
+          }) => {
+            event.preventDefault();
+            setCoordinates({ x: event.pageX, y: event.pageY });
+          }}
+          onClick={() => setCoordinates({ x: -1, y: -1 })}
+          style={{ flex: 3, textAlign: "center" }}
+        >
+          <Canvas id="ub-avatar" />
+        </Box>
 
-      <Group style={{ flex: 1 }} position="center">
-        <Button onClick={downloadAvatar} rightIcon={<Download />}>
-          Download
-        </Button>
-      </Group>
-    </Stack>
-  )
-}
+        <Group style={{ flex: 1 }} position="center">
+          <Button onClick={downloadAvatar} rightIcon={<Download />}>
+            Download
+          </Button>
+        </Group>
+      </Stack>
+      {coordinates.x !== -1 && <ContextMenu {...coordinates} />}
+    </React.Fragment>
+  );
+};
